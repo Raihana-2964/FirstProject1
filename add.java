bff492afaaa447a1d84ea64e39c7ca84e8f0a5b4
Raihana-2964 <148611059+Raihana-2964@users.noreply.github.com>
2024-05-25 14:47:58 +0530

    Update add.java

--- add.java
+++ add.java
@@ -11,8 +11,7 @@ public class MultiplyExample {
          c= a*b; //integer number to keep the result of multiplication
 >>>>>>> 7d5a49afe1b9a34fdcfc923d66113d3b8fc18ee8
 =======
-        
->>>>>>> 336f3c7efd48b1ebf7d92b4ca381b93a9257cffc
+    
         System.out.println("5*58 = " + c);
     }
  }
